2110e82c5a568a25825713cd3b3681ad846c1eda
Douglas Berkley <douglasmberkley@gmail.com>
2023-02-17 10:41:21 +0900

    added photo to alien

--- app/models/alien.rb
+++ app/models/alien.rb
@@ -3,4 +3,5 @@ class Alien < ApplicationRecord
   has_many :mutations, dependent: :destroy
   has_many :powers, through: :mutations   # yann.powers
   validates :name, presence: true, uniqueness: true
+  has_one_attached :photo # alien.photo
 end
